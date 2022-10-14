37791fb799c61954b14f21ea89b9a880b61b7d65
Nathan Matevossian <58575162+Nakuma2000@users.noreply.github.com>
2022-10-14 13:26:57 -0700

    Completing SearchEngine.java
    Edits made to SearchEngine.java to make it run properly, screenshots taken and added to the folder 'lab2pics' for use in the lab report.

--- SearchEngine.java
+++ SearchEngine.java
@@ -2,41 +2,57 @@ import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
 
-class Handler implements URLHandler {
+class Handler implements URLHandler 
+{
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
     ArrayList<String> word = new ArrayList<String>();
     String s1 = "Default";
     
-    public String handleRequest(sURI url) {
-        String list;
-        if (url.getPath().equals("/")) {
-            for(int i = 0; i < word.size(); i++){
+    public String handleRequest(URI url) 
+    {
+        String list = "Words: \n";
+        if (url.getPath().equals("/")) 
+        {
+            for(int i = 0; i < word.size(); i++)
+            {
                 String curr = word.get(i);
                 list += "\n" + curr;
             }
             return list;
-        } else {
+        } 
+        else 
+        {
             System.out.println("Path: " + url.getPath());
-            if (url.getPath().contains("/add")) {
+            if (url.getPath().contains("/add")) 
+            {
                 String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("s")) {
+                if (parameters[0].equals("s")) 
+                {
                     word.add(parameters[1]);
-                    return "Word added!"; //use stringformat
+                    return String.format("Word added!"); //use stringformat
                 }
             }
-            else {
+            else 
+            {
                 System.out.println("Path: " + url.getPath());
-                if (url.getPath().contains("/search")) {
+                if (url.getPath().contains("/search")) 
+                {
+                    String searchResult = "";
                     String[] para = url.getQuery().split("=");
-                    if (para[0].equals("s")) {
+                    if (para[0].equals("s")) 
+                    {
                         String searchInput = para[1];
-                        int index = 0;
-                        for (int i = 0; i < word.size(); i++){
+                        for (int i = 0; i < word.size(); i++)
+                        {
                             if (word.get(i).contains(searchInput))
+                            {
                                 System.out.println(word.get(i));
+                                searchResult += String.format(word.get(i) + "\n");
+                            }
                         }
                     }
+                    return String.format(searchResult +"\nSearch Complete!");
                 }
             }
             return "404 Not Found!";
@@ -45,8 +61,10 @@ class Handler implements URLHandler {
 }
 
 class SearchEngine {
-    public static void main(String[] args) throws IOException {
-        if(args.length == 0){
+    public static void main(String[] args) throws IOException 
+    {
+        if(args.length == 0)
+        {
             System.out.println("Missing port number! Try any number between 1024 to 49151");
             return;
         }
